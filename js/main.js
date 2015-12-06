720dde99ba21d2581592e2a0a69be3a717161791
Dave Charnuska <davecharnuska@gmail.com>
2015-12-06 15:33:40 -0500

    display weather, wind, temp

--- js/main.js
+++ js/main.js
@@ -1,18 +1,30 @@
-$(document).ready(function() {
+if ("geolocation" in navigator) {
+	navigator.geolocation.getCurrentPosition(function(position) {
+	    loadWeather(position.coords.latitude+','+position.coords.longitude);
+			//load weather using your lat/lng coordinates
+	  });
+} else {
+  loadWeather('Boston, MA', '');
+}
+	
+$(document).ready(function() {	
+});
+
+function loadWeather(location, woeid) {
   $.simpleWeather({
-    location: 'Austin, TX',
-    woeid: '',
+    location: location,
+    woeid: woeid,
     unit: 'f',
     success: function(weather) {
       html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-  
+      html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      
       $("#weather").html(html);
     },
     error: function(error) {
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-});
+}
